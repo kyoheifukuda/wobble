138f7049fe72d8011cc7afdf579631b7a49b9f96
kyouheifukuda <kyouhei.fukuda0729@icloud.com>
2015-07-20 18:10:55 +0900

    Create DetuneSound and KeepSound

--- app/scripts/audio.js
+++ app/scripts/audio.js
@@ -1,29 +1,49 @@
-var AudioContext = window.AudioContext;
-var context      = new AudioContext();
+var AudioContext = window.AudioContext|| window.webkitAudioContext|| false;
+
+if (AudioContext) {
+    var context = new AudioContext;
+} else {
+    alert("Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox");
+}    var context      = new AudioContext();
+
+window.addEventListener('devicemotion', function(event){
+        beta = event.accelerationIncludingGravity.y;
+        beta2 = event.acceleration.y;
+        oscillator.detune.value = beta*-10;
+        biquadFilter.frequency.value = beta*-10;
+
+}, false);
 
 
-
-window.addEventListener('deviceorientation', function(event){
-    document.getElementById('beta').innerHTML = event.beta;
-});
-
-var getfrec,
-    frec;
-window.setInterval(function(){
-  getfrec = document.getElementById('beta').innerHTML;
-  frec = Math.floor(getfrec);
-  Setup();
-  console.log(frec);
-  return frec;
-},100);
-
-function Setup() {
-    var t0 = context.currentTime;
-        var oscillator = context.createOscillator();
-        oscillator.type = 'triangle';
-        oscillator.frequency.value = 200;
-        oscillator.detune.value = frec*100;
-        oscillator.connect(context.destination);
-        oscillator.start(t0);
-        oscillator.stop(t0 + 0.5);
+function Keep() {
+  var oscillator = context.createOscillator();
+  var gainNode = context.createGain();
+  gainNode.gain.value = 5;
+  oscillator.connect(gainNode);
+  gainNode.connect(context.destination);
+  oscillator.type = 'triangle';
+  oscillator.frequency.value = 150;
+  oscillator.detune.value = 0;
+  oscillator.start(0);
+  // oscillator.stop(5);
 }
+
+
+var oscillator = context.createOscillator();
+var type      = oscillator.type;
+var biquadFilter = context.createBiquadFilter();
+    biquadFilter.type = "allpass";
+    biquadFilter.gain.value = 25;
+var gainNode = context.createGain();
+gainNode.gain.value = 5;
+function Detune() {
+  oscillator = context.createOscillator();
+  oscillator.connect(biquadFilter);
+  biquadFilter.connect(gainNode);
+  gainNode.connect(context.destination);
+  oscillator.type            = 'triangle';
+  oscillator.frequency.value = 150;
+  oscillator.start(0);
+  // oscillator.stop(5);
+}
+
